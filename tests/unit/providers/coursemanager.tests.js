4244dc0c7884443763acf0a70c6a5d79358dfd46
Young Suk Ahn <ys.ahnpark@gmail.com>
2015-03-14 21:06:05 -0400

    Course creation logic

---
+++ tests/unit/providers/coursemanager.tests.js
@@ -0,0 +1,23 @@
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var utils = require('../../lib/utils');
+
+
+// Library under test
+var courseManager = require('../../providers/coursemanager').getManager();
+
+
+describe('CourseManager', function () {
+
+	courseManager
+
+	beforeEach('', function (done) {
+
+	});
+	it('', function (done) {
+
+	});	 
+});
+
