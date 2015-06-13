b7d6eed57b1980c959ce927ebf4fe6c8d72ad2a2
Young Suk Ahn <ys.ahnpark@gmail.com>
2015-06-13 08:48:52 -0400

    2-Unit_tests

---
+++ test/spec/providers/coursemanager.tests.js
@@ -0,0 +1,23 @@
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var utils = require('../../../lib/utils/utils');
+
+
+// Library under test
+//var courseManager = require('../../providers/coursemanager').getManager();
+
+
+describe('CourseManager', function () {
+
+	beforeEach(function () {
+
+	});
+
+
+	it('should', function (done) {
+		done();
+	});	 
+});
+
---
+++ test/spec/providers/defaultprovider.tests.js
@@ -0,0 +1,25 @@
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+//var utils = require('../../lib/utils/utils');
+
+
+// Library under test
+//var courseManager = require('../../providers/defaultprovider').getManager();
+
+
+describe('DefaultProvider', function () {
+
+	beforeEach(function () {
+
+	});
+
+	describe('Adding resource', function () {	
+	
+		it('should', function (done) {
+			done();
+		});	
+	});
+});
+
